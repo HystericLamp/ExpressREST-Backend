08890497ca3987696f141effebf41ce955bbc0a9
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2024-12-16 08:36:56 -0700

    Update Readme.md and refine setup process

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ### Setup
 1. Install node and npm
 2. In the project directory, open a command line and type "npm install"
-3. Next, run command "npm dbinit" to initialize the database
-4. To run the app type "npm run start"
+3. Next, run command "npm run dbinit" to initialize the database
+4. To run the app, type "npm run start"
 5. Server is now live at http://localhost:8000/
+6. To test REST endpoints, Postman was used
 
 ### Schema
 [Download Database Schema](https://github.com/HystericLamp/TLTHP-Part1/blob/main/TLTHP%20Database%20Schema.pdf)
--- src/run-init-sql.js
+++ src/run-init-sql.js
@@ -1,8 +1,15 @@
 const sqlite3 = require('sqlite3').verbose();
+
+// Instantiate the FileSystem and create 'data' folder
 const fileSystem = require('fs');
+const dir = './src/data/';
+
+if (!fileSystem.existsSync(dir)){
+    fileSystem.mkdirSync(dir);
+}
 
 // Connect to SQLite database (or create it if it doesn't exist)
-const db = new sqlite3.Database('./src/data/tlthpDB.sqlite', (err) => {
+const db = new sqlite3.Database(dir+'tlthpDB.sqlite', (err) => {
     if (err) {
         console.error('Error connecting to database:', err.message);
     } else {
